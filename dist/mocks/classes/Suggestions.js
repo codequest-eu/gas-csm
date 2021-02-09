19fb80876dfcf3a7a3b716a92287d7b137a5d1d1
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2021-02-09 13:03:16 +0100

    Build project

--- dist/mocks/classes/Suggestions.js
+++ dist/mocks/classes/Suggestions.js
@@ -3,6 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Suggestions = void 0;
 const BaseClass_1 = require("../../base/BaseClass");
 class Suggestions extends BaseClass_1.BaseClass {
+    constructor() {
+        super();
+        this._data.suggestions = [];
+    }
     addSuggestions(suggestions) {
         suggestions.forEach(s => {
             this.addSuggestion(s);
--- types/mocks/classes/Suggestions.d.ts
+++ types/mocks/classes/Suggestions.d.ts
@@ -3,6 +3,7 @@ export interface Suggestions extends BaseClassData {
     suggestions: string[];
 }
 export declare class Suggestions extends BaseClass<Suggestions> {
+    constructor();
     addSuggestions(suggestions: string[]): this;
     addSuggestion(suggestion: string): void;
 }
